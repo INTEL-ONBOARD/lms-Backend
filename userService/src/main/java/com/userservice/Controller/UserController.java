27d286e19ac58988bad2a04acf2b7ab5015b4cf4
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-09-24 11:34:11 +0530

    Swagge UI End-point Added

--- userService/src/main/java/com/userservice/Controller/UserController.java
+++ userService/src/main/java/com/userservice/Controller/UserController.java
@@ -3,10 +3,20 @@ package com.userservice.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+
+    @ApiIgnore
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
 
     @GetMapping("/info")
     public String userInfo() {
