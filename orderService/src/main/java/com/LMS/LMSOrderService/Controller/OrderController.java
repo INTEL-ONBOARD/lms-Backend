06b815b966b1934c5aaf78c4fd9f709f3d0829f5
Chalana Devinda <86785480+RealChAuLa@users.noreply.github.com>
2024-09-25 18:38:42 +0530

    Initial Commit

---
+++ orderService/src/main/java/com/LMS/LMSOrderService/Controller/OrderController.java
@@ -0,0 +1,52 @@
+package com.LMS.LMSOrderService.Controller;
+
+import com.LMS.LMSOrderService.Data.Order;
+import com.LMS.LMSOrderService.Data.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/order")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @ApiIgnore
+    @RequestMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+    @GetMapping("/info")
+    public String orderInfo() {
+        return "This is the order service.";
+    }
+
+    @PostMapping("/order")
+    public int createOrder(@RequestBody Order order) {
+        Order savedOrder = orderRepository.save(order);
+        return savedOrder.getOrder_id();
+    }
+
+    @GetMapping("/order/{id}")
+    public Order getOrderById(@PathVariable("id") int id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/order")
+    public Order updateOrder(@RequestBody Order order) {
+        return orderRepository.save(order);
+    }
+
+    @DeleteMapping("/order/{id}")
+    public String deleteOrder(@PathVariable("id") int id) {
+        orderRepository.deleteById(id);
+        return "Order with id: " + id + " deleted";
+    }
+
+}
